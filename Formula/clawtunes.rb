ffd7247e1d0e22c84eea3ee182323b7f0376aeb7
Forketyfork <forketyfork@icloud.com>
2026-02-05 20:07:04 +0100

    fix: restore click resource in clawtunes formula

--- Formula/clawtunes.rb
+++ Formula/clawtunes.rb
@@ -14,7 +14,7 @@ class Clawtunes < Formula
   depends_on "python@3.12"
 
   resource "click" do
-    url "https://github.com/forketyfork/clawtunes/archive/refs/tags/v0.2.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
     sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
   end
 
